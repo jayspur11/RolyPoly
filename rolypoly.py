5651e2b5584bb42f68e37841cd1850363f818b94
Jaysen Spurlock <jayspur11@gmail.com>
2020-11-10 18:06:53 -0800

    Add auth stuff

--- rolypoly.py
+++ rolypoly.py
@@ -1,10 +1,12 @@
 import discord
+import json
 
 class RolyPoly(discord.Client):
     async def on_ready(self):
         print("RolyPoly reporting for duty!")
 
 if __name__ == "__main__":
-    # TODO: Need to pass auth stuff here.
+    with open("auth.json", "r") as auth_file:
+        auth = json.load(auth_file)
     bot = RolyPoly()
-    bot.run()
+    bot.run(auth["discord-token"])
