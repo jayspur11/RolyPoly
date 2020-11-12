99f3dd077b66823fa9037babb4cc33a59a214c95
Jaysen Spurlock <jayspur11@gmail.com>
2020-11-11 19:02:21 -0800

    Extract potential commands

--- rolypoly.py
+++ rolypoly.py
@@ -1,9 +1,17 @@
 import discord
 import json
+import re
 
 class RolyPoly(discord.Client):
     async def on_ready(self):
         print("RolyPoly reporting for duty!")
+
+    async def on_message(self, message):
+        if not self.user.mentioned_in(message):
+            return
+        # drop all refs in the message
+        command = re.sub(r"<..\d+>", "", message.content).strip()
+        
 
 if __name__ == "__main__":
     with open("auth.json", "r") as auth_file:
