ad35aa92cdd1531eb5f41afc4f438847a4453bdd
Jaysen Spurlock <jayspur11@gmail.com>
2020-11-11 21:01:25 -0800

    prepare to handle commands

--- rolypoly.py
+++ rolypoly.py
@@ -9,9 +9,20 @@ class RolyPoly(discord.Client):
     async def on_message(self, message):
         if not self.user.mentioned_in(message):
             return
-        # drop all refs in the message
-        command = re.sub(r"<..\d+>", "", message.content).strip()
-        
+        async with message.channel.typing():
+            # drop all refs in the message
+            command = re.sub(r"<..\d+>", "", message.content).split()
+            role_name = ' '.join([word.capitalize() for word in command[1:]])
+            channel_name = '-'.join([word.lower() for word in command[1:]])
+            if "join" == command[0]:
+                # TODO: find the appropriate role and assign it
+                pass
+            elif "register" == command[0]:
+                # TODO: create a new group & role, if it doesn't already exist
+                pass
+            else:
+                # TODO: send an error message
+                pass
 
 if __name__ == "__main__":
     with open("auth.json", "r") as auth_file:
